b9c04ec681623dda8154ae93aa8af651fef31e06
Konstantinos Parasyris <parasyris1@llnl.gov>
2022-08-30 13:37:19 -0700

    Initial commit

---
+++ include/utilities.hpp
@@ -0,0 +1,99 @@
+#pragma once 
+template<class T>
+class max_OP{
+  public:
+  static inline T OP(T& v1, T& v2){
+      if ( v2 < v1 )
+        return v1;
+      return v2;
+  }
+
+  template<class P>
+  static bool validate(T val, P correct){
+    if ( val == static_cast<T>(correct) )
+      return true;
+    return false; 
+  }
+
+  static T init(){
+    return std::numeric_limits<T>::min(); 
+  }
+
+  static T init(long i){
+    return static_cast<T>(i);
+  }
+};
+
+template<class T>
+class min_OP{
+  public:
+  static inline T OP(T& v1, T& v2){
+      if ( v2 > v1 )
+        return v1;
+      return v2;
+  }
+
+  template<class P>
+  static bool validate(T val, P val1){
+    if ( val == 0 )
+      return true;
+    return false; 
+  }
+
+  static T init(){
+    return std::numeric_limits<T>::max(); 
+  }
+
+  static T init(long i){
+    return static_cast<T>(i);
+  }
+};
+
+template<class T>
+class add_OP{
+  public:
+  static inline T OP(T& v1, T& v2){
+      return v1 + v2;
+  }
+
+  template<class P>
+  static bool validate(T val, P val1){
+    T tmp = static_cast<T>(val1);
+    T  S = tmp/2.0 * (tmp-1);
+    if ( val == S )
+      return true;
+    return false; 
+  }
+
+  static T init(){
+    return 0; 
+  }
+
+  static T init(long i){
+    return static_cast<T>(i);
+  }
+};
+
+template<class T>
+class mult_OP{
+  public:
+  static inline T OP(T& v1, T& v2){
+      return v1 * v2;
+  }
+
+  template<class P>
+  static bool validate(T val, P val1){
+    if ( val == 1.0 )
+      return true;
+    return false; 
+  }
+
+  static T init(){
+    return 1; 
+  }
+
+  static T init(long i){
+    return 1;
+  }
+};
+
---
+++ openmp/device/driver.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <chrono>
+#include <limits>
+
+#include "utilities.hpp"
+
+using namespace std;
+
+#define DTYPE @REDUCTION_TYPE@ 
+
+int main(int argc, char** argv)
+{
+  using util = @OPER_UTIL@<DTYPE>;
+  long elements = atol(argv[1])  * (1024L*1024L) / sizeof(DTYPE);
+  DTYPE *data = nullptr;
+  DTYPE out = util::init();
+
+#pragma omp target data map (alloc:data[0:elements])
+  {
+#pragma omp target teams distribute parallel for
+    for ( long i = 0 ; i < elements; i++){
+      data[i] = util::init(i);
+    }
+
+    auto start = chrono::high_resolution_clock::now();
+#pragma omp target teams distribute parallel for @OMP_REDUCTION@ 
+    for ( long i = 0 ; i < elements; i++){
+      out = util::OP(out, data[i]);
+    }
+    auto end = chrono::high_resolution_clock::now();
+    cout << "Elapsed time in milliseconds: "
+      << chrono::duration<double>(end - start).count() << " s" << endl;
+  }
+
+  std::cout << "Reduced value is:" << out <<  "\n";
+
+  if ( util::validate(out, elements) )
+    std::cout << "PASS \n";
+  else
+    std::cout << "FAIL\n";
+
+  free(data);
+
+  return 0;
+}
+
